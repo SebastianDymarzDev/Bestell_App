9720f172fa99a539dcda03ddf60e8987bf59415a
Sebastian Dymarz <s.dymarz@gmx.de>
2026-05-21 13:47:51 +0200

    add path for image in db

--- scripts/db.js
+++ scripts/db.js
@@ -2,22 +2,26 @@ let burgers = [
     {
     "name": "Veggie mushroom black burger",
     "price": 16.90,
-    "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms"
+    "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    "image": "./img/veggie_mushroom.png"
     },
     {
     "name": "All meat burger",
     "price": 15.90,
-    "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse"
+    "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+    "image": "./img/all_meat.png"
     },
     {
     "name": "Beef red burger",
     "price": 14.90,
-    "description": "Beef, Cheese, Tomatoes, Lettuce, Onion"
+    "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    "image": "./img/beef_red.png"
     },
     {
     "name": "Big chicken burger",
     "price": 15.90,
-    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper "
+    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+    "image": "./img/big_chicken.png"
     }
 ]
 
@@ -25,22 +29,26 @@ let pizza = [
     {
     "name": "Pizza Margherita",
     "price": 11.90,
-    "description": "Tomato Sauce, Mozzarella"
+    "description": "Tomato Sauce, Mozzarella",
+    "image": "./img/margherita.png"
     },
     {
     "name": "Pizza Chorizo",
     "price": 13.90,
-    "description": "Tomato slices, Mozzarella, Chorizo"
+    "description": "Tomato slices, Mozzarella, Chorizo",
+    "image": "./img/chorizo.png"
     },
     {
     "name": "Funghi",
     "price": 12.90,
-    "description": "Red onion, Olives, Button Mushrooms, Mozzarella"
+    "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
+    "image": "./img/funghi.png"
     },
     {
     "name": "Quattro Formaggi with Chicken",
     "price": 15.90,
-    "description": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano"
+    "description": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+    "image": "./img/quattro_formaggio.png"
     }
 ]
 
@@ -48,21 +56,25 @@ let salad = [
     {
     "name": "Warm beef arugula salad",
     "price": 16.90,
-    "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing"
+    "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+    "image": "./img/warm_beef.png"
     },
     {
     "name": "Mini green Salad",
     "price": 7.90,
-    "description": "Green salad, Cucumber, Carrots, Parsley, Radishes"
+    "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    "image": "./img/mini_green.png"
     },
     {
     "name": "Green Salad with sea food",
     "price": 16.90,
-    "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill"
+    "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    "image": "./img/green_salad.png"
     },
     {
-    "name": "Veagn green salad with tofu",
+    "name": "Vegan green salad with tofu",
     "price": 14.90,
-    "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts"
+    "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+    "image": "./img/vegan_green.png"
     }
 ]
