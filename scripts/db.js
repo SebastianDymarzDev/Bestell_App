deab9e0f0c90c31fa03fe3968d8804b6916618f8
Sebastian Dymarz <s.dymarz@gmx.de>
2026-05-21 09:39:26 +0200

    add Imprint, Style Landing Sextion and fill db

--- scripts/db.js
+++ scripts/db.js
@@ -1,10 +1,68 @@
-let myDishes = [
+let burgers = [
     {
-    "name": "Spaghetti",
-    "price": 12.99,
-    "description": "Nudeln mit Soße"
+    "name": "Veggie mushroom black burger",
+    "price": 16.90,
+    "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms"
     },
     {
-        
+    "name": "All meat burger",
+    "price": 15.90,
+    "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse"
+    },
+    {
+    "name": "Beef red burger",
+    "price": 14.90,
+    "description": "Beef, Cheese, Tomatoes, Lettuce, Onion"
+    },
+    {
+    "name": "Big chicken burger",
+    "price": 15.90,
+    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper "
+    }
+]
+
+let pizza = [
+    {
+    "name": "Pizza Margherita",
+    "price": 11.90,
+    "description": "Tomato Sauce, Mozzarella"
+    },
+    {
+    "name": "Pizza Chorizo",
+    "price": 13.90,
+    "description": "Tomato slices, Mozzarella, Chorizo"
+    },
+    {
+    "name": "Funghi",
+    "price": 12.90,
+    "description": "Red onion, Olives, Button Mushrooms, Mozzarella"
+    },
+    {
+    "name": "Quattro Formaggi with Chicken",
+    "price": 15.90,
+    "description": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano"
+    }
+]
+
+let salad = [
+    {
+    "name": "Warm beef arugula salad",
+    "price": 16.90,
+    "description": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing"
+    },
+    {
+    "name": "Mini green Salad",
+    "price": 7.90,
+    "description": "Green salad, Cucumber, Carrots, Parsley, Radishes"
+    },
+    {
+    "name": "Green Salad with sea food",
+    "price": 16.90,
+    "description": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill"
+    },
+    {
+    "name": "Veagn green salad with tofu",
+    "price": 14.90,
+    "description": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts"
     }
 ]
